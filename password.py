a25ba26983b25602b1874583244554832d4fd775
damunza <danidamunza@gmail.com>
2018-08-24 08:42:25 +0300

    ran the test for the initial entry of data and it  was ok

--- password.py
+++ password.py
@@ -34,3 +34,13 @@ class Credential:
             if user.first_name == first_name and user.password == password:
                 current_user = user.first_name
                 return current_user
+
+    def __init__(self,user_name,site_name,account_name,password):
+        '''
+        method that defines the properties of each credential object
+        '''
+
+        self.user_name = user_name
+        self.site_name = site_name
+        self.account_name = account_name
+        self.password = password            
